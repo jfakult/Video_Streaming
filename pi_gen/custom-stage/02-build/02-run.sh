ddc2e4cc662b997b65280684841b7cce160c672c
Graham Stewart <grst2704@colorado.edu>
2023-10-21 13:25:20 -0600

    renaming script files

---
+++ pi_gen/custom-stage/02-build/02-run.sh
@@ -0,0 +1,10 @@
+#!/bin/bash -e
+# This cleans up the boot config files
+
+#remove any auto detect lines
+sudo sed -i '/camera_auto_detect/d' /boot/config.txt
+
+#disable camera autodetect and add overlays for both the camera ande display
+echo 'camera_auto_detect=0' >> /boot.config.txt
+echo 'dtoverlay=imx708'>> /boot.config.txt
+echo 'dtoverlay=gc9a01' >> /boot.config.txt
