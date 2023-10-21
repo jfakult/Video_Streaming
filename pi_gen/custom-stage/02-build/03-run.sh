f2d2dc9ca2ad6093d1232cf44239aece90a077a6
Graham Stewart <grst2704@colorado.edu>
2023-10-21 14:28:43 -0600

    remove references to sudo

---
+++ pi_gen/custom-stage/02-build/03-run.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# URL of the release tar.gz file
+url="https://github.com/bluenviron/mediamtx/releases/download/v1.2.0/mediamtx_v1.2.0_linux_armv7.tar.gz"
+
+# Temporary directory to store the downloaded tar.gz file
+temp_dir=$(mktemp -d)
+
+# Name of the tar.gz file
+file_name=$(basename "$url")
+
+# Download the tar.gz file
+wget "$url" -O "$temp_dir/$file_name"
+
+# Extract the mediamtx binary
+tar -xzf "$temp_dir/$file_name" -C "$temp_dir" --strip-components=1 mediamtx
+
+# Move the mediamtx binary to /usr/local/bin/
+mv "$temp_dir/mediamtx" /usr/local/bin/
+
+# Set execute permission
+chmod +x /usr/local/bin/mediamtx
+
+# Clean up the temporary directory
+rm -r "$temp_dir"
