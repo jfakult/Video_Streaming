5ca059881909ebf7ac2c789d9c50082fa42a9b93
Graham Stewart <grst2704@colorado.edu>
2023-10-22 08:40:04 -0600

    fix to extract the correct mediamtx binary

--- pi_gen/custom-stage/02-build/02-run-chroot.sh
+++ pi_gen/custom-stage/02-build/02-run-chroot.sh
@@ -8,3 +8,4 @@ sed -i '/camera_auto_detect/d' /boot/config.txt
 echo 'camera_auto_detect=0' >> /boot.config.txt
 echo 'dtoverlay=imx708'>> /boot.config.txt
 echo 'dtoverlay=gc9a01' >> /boot.config.txt
+echo 'dtparam=spi=on' >> /boot.config.txt
