29e82ba0ef93f2ff894b8e597aab3b1c7e38b5f6
Graham Stewart <grst2704@colorado.edu>
2023-10-22 13:55:50 -0600

    forgot a few minor things

--- pi_gen/custom-stage/02-build/02-run-chroot.sh
+++ pi_gen/custom-stage/02-build/02-run-chroot.sh
@@ -5,7 +5,7 @@
 sed -i '/camera_auto_detect/d' /boot/config.txt
 
 #disable camera autodetect and add overlays for both the camera ande display
-echo 'camera_auto_detect=0' >> /boot.config.txt
-echo 'dtoverlay=imx708'>> /boot.config.txt
-echo 'dtoverlay=gc9a01' >> /boot.config.txt
-echo 'dtparam=spi=on' >> /boot.config.txt
+echo 'camera_auto_detect=0' >> /boot/config.txt
+echo 'dtoverlay=imx708'>> /boot/config.txt
+echo 'dtoverlay=gc9a01' >> /boot/config.txt
+echo 'dtparam=spi=on' >> /boot/config.txt
