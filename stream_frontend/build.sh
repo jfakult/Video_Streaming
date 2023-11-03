e746cb85e7f69323a606282a188dd83aa539b772
Jake <jfakult15@gmail.com>
2023-11-03 12:51:04 -0600

    fixed unmask issue

--- stream_frontend/build.sh
+++ stream_frontend/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-cp /etc/nginx/nginx.conf ..
-cp /etc/dnsmasq.conf ..
+#cp /etc/nginx/nginx.conf ..
+#cp /etc/dnsmasq.conf ..
 
 npx quasar build && \
 cp -r dist/spa/* /usr/share/nginx/html/stream/
