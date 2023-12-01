e87d0f97d2cab35a3c0d1e4c3a484e6190b52a15
graham <graham.stewart@colorado.edu>
2023-11-30 21:43:49 -0700

    cleaning up startup scripts

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
-
+touch /tmp/hello
 BASE="/home/pi/Video_Streaming"
 
 rfkill unblock all
 
 # Startup python backend and websockets
 cd $BASE/backend
-nohup python server.py &
+python server.py 
 
-cd $BASE/scripts
-nohup ./stream.sh &
-nohup ./logo.sh &
